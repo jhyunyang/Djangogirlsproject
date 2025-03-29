ecaa11c1a2cb4c637a3d15eeea4ed7be48178fb5
jhyunyang <jhyunyang@outlook.com>
2025-03-29 19:43:57 +0000

    I added html page

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -122,7 +122,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-if 'CODESPACE_NAME' in os.environ:
-    codespace_name = os.getenv("CODESPACE_NAME")
-    codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
-    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
